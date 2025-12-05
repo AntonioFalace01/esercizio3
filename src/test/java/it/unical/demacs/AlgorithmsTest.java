f885e62c6f69d3bd543f0fc044bf2698955e1adf
AntonioFalace01 <falaceantonio290@gmail.com>
2025-12-05 12:20:12 +0100

    metodo per testare che inserito un null, la risposta sarà un eccezione

--- src/test/java/it/unical/demacs/AlgorithmsTest.java
+++ src/test/java/it/unical/demacs/AlgorithmsTest.java
@@ -1,6 +1,9 @@
 package it.unical.demacs;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AlgorithmsTest {
 
@@ -9,6 +12,13 @@ public class AlgorithmsTest {
     @BeforeEach
     public void setUp() {
         algorithms = new Algorithms();
+    }
+
+    @Test
+    public void inputNullArray(){
+        assertThrows( IllegalArgumentException.class, () -> {)
+            algorithms.sort(null);
+        });
     }
 
 
