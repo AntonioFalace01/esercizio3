a297118e20682bf91a81b53f689d6034e2f50fb4
AntonioFalace01 <falaceantonio290@gmail.com>
2025-12-05 12:15:16 +0100

    aggiunta di beofre

---
+++ src/test/java/it/unical/demacs/AlgorithmsTest.java
@@ -0,0 +1,15 @@
+package it.unical.demacs;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class AlgorithmsTest {
+
+    private Algorithms algorithms;
+
+    @BeforeEach
+    public void setUp() {
+        algorithms = new Algorithms();
+    }
+
+
+}
